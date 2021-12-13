ed52655e0ab989c4a06f8ac26e4ffa6ee469bc2f
Arvind-4 <lordarvindthegamer@gmail.com>
2021-12-13 19:49:25 +0530

    Added _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from 'react'
+import Link from 'next/link'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='stylesheet'
+            href='https://pro.fontawesome.com/releases/v5.10.0/css/all.css'
+            integrity='sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
