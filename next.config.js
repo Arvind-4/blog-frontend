396adb44103fd36537b6b014394315b0044e7225
Arvind <pingarvindforquestions@gmail.com>
2024-07-26 05:55:02 +0000

    fix: .mjs -> .js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const { withContentlayer } = require('next-contentlayer')
+const nextConfig = {
+  reactStrictMode: true,
+}
+module.exports = withContentlayer(nextConfig)
--- next.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-import { withContentlayer } from 'next-contentlayer'
-const nextConfig = {}
-
-export default withContentlayer(nextConfig)
